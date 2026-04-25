3171cb59a4729b8977927e94ffd0128c8f89f58c
Avanish7974 <avanishsingh4318@gmail.com>
2026-04-25 12:20:09 +0530

    fix render database initialization

--- kpit-agile-project/backend/app.py
+++ kpit-agile-project/backend/app.py
@@ -40,6 +40,8 @@ def generate_report():
             print("===========================\n")
 
 
+
+
 @app.route("/")
 def home():
     return "Agile Project Manager Backend Running 🚀"
@@ -50,6 +52,9 @@ if __name__ == "__main__":
     worker = threading.Thread(target=generate_report)
     worker.daemon = True
     worker.start()
+
+    with app.app_context():
+        db.create_all()
 
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
