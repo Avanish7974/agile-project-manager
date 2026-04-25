4a94b5659e3c3d73551f6ea4cc8c3723a6063214
Avanish7974 <avanishsingh4318@gmail.com>
2026-04-25 12:16:43 +0530

    fix render server port

--- kpit-agile-project/backend/app.py
+++ kpit-agile-project/backend/app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import threading
 import time
+import os
 
 app = Flask(__name__)
 
@@ -39,10 +40,16 @@ def generate_report():
             print("===========================\n")
 
 
+@app.route("/")
+def home():
+    return "Agile Project Manager Backend Running 🚀"
+
+
 if __name__ == "__main__":
 
     worker = threading.Thread(target=generate_report)
     worker.daemon = True
     worker.start()
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
